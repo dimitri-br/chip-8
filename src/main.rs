a68b6cccfdaa25f8e41032671d84e7430c9587bf
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2020-06-21 16:16:31 +0100

    Simplified code, moved execute_cycle to cpu

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ fn main(){
     println!("Hello!\nWelcome to the chip-8 emulator.\nIt currently isn't working :(\nPlease check later!");
     let mut cpu = CPU::new().unwrap();
     cpu = load_rom(cpu).unwrap();
-    cpu.set_opcode(cpu.get_program_counter().unwrap()).unwrap();
-
     loop{
         cpu = emulate_cycle(cpu).unwrap();
     }  
